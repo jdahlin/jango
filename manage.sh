9ef39db0a3025674a5c6dbc44d139b0c275a75c9
Johan Dahlin <johan@gnome.org>
2009-06-23 17:07:18 -0300

    Make it possible to run outside of source dir

--- manage.sh
+++ manage.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 export GJS_DEBUG_TOPICS="JS LOG"
 export GJS_DEBUG_OUTPUT=stderr
-export GJS_PATH=`pwd`:`pkg-config --variable=datarootdir gjs-1.0`/gjs-1.0
+export GJS_PATH=`pwd`:`dirname $0`:`pkg-config --variable=datarootdir gjs-1.0`/gjs-1.0
 export LD_LIBRARY_PATH=`pkg-config --variable=mozjslibdir gjs-1.0`
-gjs-console jango/manage.js $@
+gjs-console `dirname $0`/jango/manage.js $@
